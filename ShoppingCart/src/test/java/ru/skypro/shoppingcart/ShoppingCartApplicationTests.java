a63524df08e9f3fa0985b8ea6a654a8d2889c2f9
z.razzhivin <z.razzhivin@tinkoff.ru>
2023-12-09 12:15:40 +0300

    init ShoppingCard project

---
+++ ShoppingCart/src/test/java/ru/skypro/shoppingcart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.shoppingcart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
