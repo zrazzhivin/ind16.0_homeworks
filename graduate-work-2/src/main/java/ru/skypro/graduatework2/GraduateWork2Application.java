defef7da6ec2db14f15dc577906ae11df0e05179
zrazzhivin <zrazzhivin@gmail.com>
2024-01-13 12:05:10 +0300

    create graduate work 2 project

---
+++ graduate-work-2/src/main/java/ru/skypro/graduatework2/GraduateWork2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.graduatework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateWork2Application.class, args);
+	}
+
+}
---
+++ graduate-work-2/src/test/java/ru/skypro/graduatework2/GraduateWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.graduatework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
