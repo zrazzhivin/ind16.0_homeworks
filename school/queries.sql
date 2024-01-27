eb1e5187f284e6bac4912ac2da77cdc1cd02b0ea
zrazzhivin <zrazzhivin@gmail.com>
2024-01-27 13:08:15 +0300

    added sql queries

---
+++ school/queries.sql
@@ -0,0 +1,9 @@
+select * from student where age > 10 and age < 27;
+
+select name from student;
+
+select * from student where name like '%o%';
+
+select * from student where age < id;
+
+select * from student order by age;
