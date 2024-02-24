9a57b8e6cc327f69b866cfb77853f81b6334c77b
z.razzhivin <z.razzhivin@tinkoff.ru>
2024-02-24 12:51:09 +0300

    add separate profile

---
+++ school/src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -0,0 +1,17 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+
+    @Value("${server.port}")
+    private Integer port;
+
+    @GetMapping("port")
+    public int getPort() {
+        return port;
+    }
+}
